50172a49854096ba290f6d41ad6dc8d5c7899321
Su948 <752039771@qq.com>
2026-09-03 01:01:50 +0800

    SEO: Centralize operational response claims

---
+++ src/lib/operational-claims.ts
@@ -0,0 +1,5 @@
+export const operationalClaims = {
+  response:
+    "We aim to respond to business inquiries within 24 hours on working days.",
+  responseShort: "Typically within 24 hours on working days",
+} as const;
